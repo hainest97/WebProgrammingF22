a295387fd1c47b5cdb90872c96df3b7d9eb90b26
tisthes <thaines@altny.net>
2022-11-03 15:25:11 -0400

    Creating pipeline and serving static files

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const productsController = require('./controllers/products');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  next();
+})
+app.use('/',express.static('./client/dist'));
+
 app.get('/', (req, res) => {
   // res.statusCode = 200;
   // res.setHeader('Content-Type', 'text/plain');
@@ -12,12 +18,27 @@ app.get('/', (req, res) => {
 
   res.status(200).send('Hello World');
 })
-.use('/api/v1/products', productsController)
+  .get('/error', (req,res) => {
+    SSS.PORT;
+  })
+  .use('/api/v1/products', productsController);
+
+app.get('*',(req,res) => {
+  res.sendFile('index.html',{root: './client/dist'});
+})
+
+app.use((err,req,res,next)=>{
+  console.log(err);
+  res.status(err.httpCode ?? 500).send({
+    message: err.msg ?? 'Something broke!',
+    status: err.httpCode ?? 500
+  });
+})
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
 
-function nn(paramName){
+function nn(paramName) {
 
 }
 const nnn = function (paramName) {
@@ -25,7 +46,7 @@ const nnn = function (paramName) {
 }
 
 const obj = {
-  vvv(){
+  vvv() {
 
   }
 }
