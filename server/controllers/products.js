5cf1995e140797263be2d8c45aea26e01d033d4f
tisthes <thaines@altny.net>
2022-12-01 14:13:14 -0500

    Mongo

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -4,17 +4,29 @@ const products = require('../models/products');
 const app = express.Router();
 
 app
-  .get('/',(req,res) => {
-      res.status(200).send(products.getProducts());
-})
-  .get('/:id', (req,res) => {
-      // res.send(req.params.id);
-      const product = products.getProduct(+req.params.id);
-      if(product){
-        res.status(200).send(product);
-      } else {
-        res.status(200).send("Product not found");
-      }
-})
+  .get('/', (req, res, next) => {
+    products.getProducts()
+      .then(x => res.status(200).send(x))
+      .catch(next);
+    // res.status(200).send(products.getProducts());
+  })
+  .get('/:id', (req, res, next) => {
+    products.getProduct(req.params.id)
+      .then(product => {
+        if (product) {
+          res.status(200).send(product);
+        } else {
+          res.status(200).send("Product not found");
+        }
+      })
+      .catch(next);
+    // // res.send(req.params.id);
+    // const product = products.getProduct(+req.params.id);
+    // if(product){
+    //   res.status(200).send(product);
+    // } else {
+    //   res.status(200).send("Product not found");
+    // }
+  })
 
 module.exports = app;
